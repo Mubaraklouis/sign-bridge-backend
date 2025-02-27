b558984094155b94832c292389aad4d4621f5775
abdo-aleer <juniorab444@gmail.com>
2025-02-28 00:19:09 +0200

    added two endpoints for login, logout

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const jwt = require("jsonwebtoken");
-const User = require("./models/user");
 
+const connectDB = require("./config/db");
+const User = require("./models/user");
+const tokenGeneration = require("./middleware/generateToken");
+
+// const authenticate = require("./middleware/authMiddleware");
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const jsonParser = bodyParser.json();
 
 app.get("/", async (req, res) => {
   const message = "Hello World";
@@ -14,11 +22,39 @@ app.get("/", async (req, res) => {
   res.send(message);
 });
 
-app.post("/api/login", (req, res) => {
-  console.log("trying to login");
-  res.json({ status: 200 });
+// end-point for login in users
+app.post("/api/login", jsonParser, async (req, res) => {
+  // here I want to check if there's user in the db
+  const data = JSON.stringify(req.body);
+
+  try {
+    const userDB = await User.findOne({ email: data.email });
+    if (userDB == null) {
+      res.sendStatus(404);
+    } else {
+      console.log(userDB);
+      // generateAccessToken
+      const token = tokenGeneration.generateAccessToken({
+        email: userDB.email,
+      });
+      res.json({ accessToken: token });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// end-point for registering users
+app.post("/api/register", jsonParser, async (req, res) => {
+  const data = req.body;
+
+  const user = await mongoose.User.create(data);
+  user.save();
+
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
 });
+app.use(bodyParser.json());
