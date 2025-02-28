b74fa7bc44ad6e037064b9c07d714cb925c312af
abdo-aleer <juniorab444@gmail.com>
2025-02-28 05:51:02 +0200

    Modified the User schema to hash the password before saving it to db

--- models/user.js
+++ models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String, // String is shorthand for {type: String}
-  email: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   JoinDate: { type: Date, default: Date.now },
 });
 
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
 module.exports = mongoose.model("User", userSchema);
